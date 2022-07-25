88bee4077faa7d043c00df03ff5600ad771b3f1d
hipale <hhemmhilb@gmail.com>
2022-07-25 18:48:43 +0200

    Add new button Make function for clear button Change the script so a is based in operation function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let scr = document.querySelector("#screen");
 let a;
 let sum = 0;
 scr.innerHTML = "";
+let clear = document.querySelector("#clearButt");
+let prop = document.querySelectorAll(".prop");
 let bottomScr = document.querySelector("#bottomScreen");
 const tile = document.querySelectorAll(".tile");
 for (let i = 0; i < tile.length; i++) {
@@ -12,24 +14,43 @@ function addNum(e) {
 scr.innerHTML += e.target.innerHTML;
 }
 function add() {
-a = " + ";
-scr.innerHTML += " + "
-}
-function sub() {
-a = " - "
-scr.innerHTML += " - "
-}
-function operate() {
-  let numCount = scr.innerHTML.split(a);
-if (a == " + ") {
-    for(let i = 0; i < numCount.length; i++) {
-        sum += parseInt(numCount[i]);
+    for (let i = 0; i < numCount.length; i++) {
+        return sum += numCount[i];
     }
-} else if (a == " - ") {
-sum = numCount[0] - numCount[1];
+bottomScr.innerHTML = sum;
 }
- 
- console.log(sum);
+
+function sub() {
+a = " - ";
+scr.innerHTML += " - ";
+}
+function multi() {
+    a = " * ";
+    scr.innerHTML = " * ";
+}
+function operate(e) {
+    if (scr.innerHTML !== "") {
+        a = e.target.innerHTML;
+        console.log(a);
+        scr.innerHTML += e.target.innerHTML;
+        let numCount = scr.innerHTML.split(a);
+    } else {
+        scr.innerHTML = "";
+    }
+
+  if(e.target.innerHTML == "=") {
+if (a == "+") {
+    add()
+    }
+ else if (a == "-") {
+sub()
+}  else if (a == "*") {
+    multi()
+}
  bottomScr.innerHTML = sum;
  scr.innerHTML = "";
 }
+}
+clear.addEventListener("click", function() { bottomScr.innerHTML = "";
+scr.innerHTML = "";
+})
