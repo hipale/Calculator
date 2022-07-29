43e989ee0500ed24561a92cf66cef05a639346bd
hipale <hhemmhilb@gmail.com>
2022-07-29 18:37:53 +0200

    Fix pi function and dot function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let scr = document.querySelector("#screen");
-let a;
+let a = "";
 let y = 0;
 let sum = 0;
 let numCount = [0];
@@ -11,15 +11,13 @@ let reverseButt = document.querySelector("#reverseNum");
 const tile = document.querySelectorAll(".tile");
 let dot = document.querySelector("#dot");
 let pi = document.querySelector("#pi");
-pi.addEventListener("click", function() {
-    scr.innerHTML += 3.14;
-});
+let piNum = 0;
 for (let i = 0; i < tile.length; i++) {
     tile[i].addEventListener("click", addNum);
 }
 function addNum(e) {
     const arrayPos = numCount.length - 1;
-    if(numCount[arrayPos] == "") {
+    if(numCount[arrayPos] == "" && piNum == 0) {
         dot.addEventListener("click", fraction);
     }
     scr.innerHTML += e.target.innerHTML;
@@ -30,7 +28,7 @@ for (let i = 0; i < prop.length; i++) {
     prop[i].addEventListener("click", operate);
 }
 function add() {
-    sum = (parseFloat(numCount[0]) + parseFloat(numCount[1])).toFixed(1);
+    sum = (parseFloat(numCount[0]) + parseFloat(numCount[1]));
 }
 function sub() {
     sum = parseFloat(numCount[0]) - parseFloat(numCount[1]);
@@ -51,15 +49,21 @@ function operate(e) {
         a = e.target.innerHTML;
         scr.innerHTML += e.target.innerHTML;
         dot.addEventListener("click", fraction);
+        pi.addEventListener("click", addPi);
+        piNum = 0;
     } else if (y < 1) {
         scr.innerHTML = "";
     }
     choseMethod()
+    sum = sum.toFixed(2);
     if (numCount.length == 1) {
         numCount[1] = 0;
         y++;
         sum = numCount[0];
-    }
+       if(a == "") {
+        a = "+"
+       }
+    } 
     if (e.target.innerHTML == "=" || y > 1) {
         setResult()
         sum = Math.round(sum).toFixed(1);
@@ -75,6 +79,8 @@ clear.addEventListener("click", function () {
     bottomScr.innerHTML = "";
     scr.innerHTML = "";
     y = 0;
+    pi.addEventListener("click", addPi);
+    piNum = 0;
 });
 reverseButt.addEventListener("click", function () {
     const arrayPos = numCount.length - 1;
@@ -106,12 +112,23 @@ function choseMethod() {
 function fraction(e) {
     scr.innerHTML += ".";
  dot.removeEventListener("click", fraction); 
+ pi.removeEventListener("click", addPi);
+ piNum = 1;
 };
 function setResult() {
-
-  
     if (sum != 0) {
         scr.innerHTML = sum;
         bottomScr.innerHTML = `${numCount[0]} ${a} ${numCount[1]}`;
-    }
+    } 
 }
+pi.addEventListener("click", addPi);
+ function addPi() {
+let arrayPos = numCount.length - 1;
+    if(numCount[arrayPos] == "") {
+        scr.innerHTML += 3.14;
+        pi.removeEventListener("click", addPi);
+        dot.removeEventListener("click", fraction);
+        piNum = 1;
+        numCount[arrayPos] += 3.14;
+    }
+ };
